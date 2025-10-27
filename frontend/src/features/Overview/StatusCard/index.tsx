ca9ad5703ee15b7aa8751a553b79c39c5ef481a9
Gray Choi <eungu0920@gmail.com>
2025-10-27 14:29:21 +0900

    edit

--- frontend/src/features/Overview/StatusCard/index.tsx
+++ frontend/src/features/Overview/StatusCard/index.tsx
@@ -1,20 +1,11 @@
-import { Flex, Progress, Box, Text } from "@radix-ui/themes";
+import { Flex, Box } from "@radix-ui/themes";
 import CardHeader from "../../../components/CardHeader";
 import Card from "../../../components/Card";
 import CardStat from "../../../components/CardStat";
 import { useAtomValue } from "jotai";
 import styles from "./statusCard.module.css";
 import { currentSlotAtom } from "../../../atoms";
-import useNextSlot from "../../../hooks/useNextSlot";
-import { voteDistanceAtom, voteStateAtom } from "../../../api/atoms";
-import {
-  failureColor,
-  headerColor,
-  mySlotsColor,
-  nextColor,
-  regularTextColor,
-  successColor,
-} from "../../../colors";
+import { headerColor } from "../../../colors";
 
 export default function SlotStatusCard() {
   return (
@@ -23,11 +14,6 @@ export default function SlotStatusCard() {
         <CardHeader text="Status" />
         <div className={styles.statRow}>
           <CurrentSlotText />
-          <NextSlotTimeText />
-        </div>
-        <div className={styles.statRow}>
-          <VotingStatusText />
-          <UpcomingSlotText />
         </div>
       </Flex>
     </Card>
@@ -47,76 +33,4 @@ function CurrentSlotText() {
       />
     </Box>
   );
-}
-
-function UpcomingSlotText() {
-  const { nextLeaderSlot } = useNextSlot();
-
-  return (
-    <CardStat
-      label="Next leader slot"
-      value={nextLeaderSlot?.toString() ?? "∞"}
-      valueColor={nextColor}
-      valueStyle={
-        nextLeaderSlot === undefined
-          ? { fontSize: "32px", lineHeight: "16px" }
-          : undefined
-      }
-    />
-  );
-}
-
-function NextSlotTimeText() {
-  const { progressSinceLastLeader, nextSlotText } = useNextSlot();
-
-  return (
-    <Flex direction="column">
-      <CardStat
-        label="Time until leader"
-        value={nextSlotText}
-        valueColor={headerColor}
-        large
-      />
-      <Progress
-        value={progressSinceLastLeader}
-        size="1"
-        className={styles.progress}
-      />
-    </Flex>
-  );
-}
-
-function VotingStatusText() {
-  const voteState = useAtomValue(voteStateAtom);
-
-  let voteColor = regularTextColor;
-  if (voteState === "voting") {
-    voteColor = successColor;
-  } else if (voteState === "non-voting") {
-    voteColor = mySlotsColor;
-  } else if (voteState === "delinquent") {
-    voteColor = failureColor;
-  }
-
-  return (
-    <CardStat
-      label="Vote Status"
-      value={voteState ?? "Unknown"}
-      valueColor={voteColor}
-    >
-      <VoteDistanceText />
-    </CardStat>
-  );
-}
-
-function VoteDistanceText() {
-  const voteDistance = useAtomValue(voteDistanceAtom);
-  const voteState = useAtomValue(voteStateAtom);
-
-  if (voteDistance == null) return null;
-  if (voteState === "delinquent") return null;
-
-  const value = voteDistance > 150 ? "> 150" : voteDistance;
-
-  return <Text className={styles.voteDistance}>{value} behind</Text>;
 }
